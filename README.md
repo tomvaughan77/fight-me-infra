5054911221074c3ba5260ef32113852a22b4857e
Tom Vaughan <tomvaughan77@gmail.com>
2023-04-15 12:48:27 +0100

    Initial commit with tf s3 backend

---
+++ README.md
@@ -0,0 +1,3 @@
+# Fight.Me Infrastructure
+
+Yep
---
+++ main.tf
@@ -0,0 +1,31 @@
+terraform {
+  backend "s3" {
+    bucket         = "fight-me-infra-terraform-state-bucket"
+    key            = "terraform.tfstate"
+    region         = "eu-west-2"
+    dynamodb_table = "terraform-state-lock"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = "eu-west-2"
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-example-bucket"
+}
+
+resource "aws_s3_bucket_ownership_controls" "my_bucket" {
+  bucket = aws_s3_bucket.my_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "my_bucket" {
+    depends_on = [aws_s3_bucket_ownership_controls.my_bucket]
+
+    bucket = aws_s3_bucket.my_bucket.id
+    acl    = "private"
+}
