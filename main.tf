d5619c492575857e93b17396c8bc32bb2ae5e8b3
Tom Vaughan <tomvaughan77@gmail.com>
2023-04-16 12:53:24 +0100

    Sod amplify, site is static + clientJs, using an s3 bucket

--- main.tf
+++ main.tf
@@ -12,61 +12,118 @@ provider "aws" {
   region = "eu-west-2"
 }
 
-provider "github" {
-  token = var.github_token
-  owner = var.github_owner
+resource "aws_s3_bucket" "fight_me_frontend" {
+  bucket = "fight-me-frontend-nextjs-app"
+  website {
+    index_document = "index.html"
+  }
 }
 
-resource "aws_amplify_app" "fight_me_frontend_amplify_app" {
-  name       = var.amplify_app_name
-  repository = "https://github.com/${var.github_owner}/${var.github_repository}"
+resource "aws_s3_bucket_ownership_controls" "fight_me_frontend_ownership_controls" {
+  bucket = aws_s3_bucket.fight_me_frontend.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
 
-  access_token = var.github_token
+resource "aws_s3_bucket_public_access_block" "fight_me_frontend_public_access_block" {
+  bucket = aws_s3_bucket.fight_me_frontend.id
 
-  enable_auto_branch_creation = true
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
 
-  auto_branch_creation_patterns = [
-    "*",
-    "*/**",
+resource "aws_s3_bucket_acl" "fight_me_frontend_acl" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.fight_me_frontend_ownership_controls,
+    aws_s3_bucket_public_access_block.fight_me_frontend_public_access_block,
   ]
 
-  auto_branch_creation_config {
-    enable_auto_build = true
-  }
-
-  custom_rule {
-    source = "</^[^.]+$|\\.(?!(css|gif|ico|jpg|js|png|txt|svg|woff|ttf|map|json)$)([^.]+$)/>"
-    status = "200"
-    target = "/index.html"
-  }
-
-  build_spec = <<EOT
-version: 1
-frontend:
-  phases:
-    preBuild:
-      commands:
-        - npx pnpm install
-    build:
-      commands:
-        - npx pnpm build
-  artifacts:
-    baseDirectory: .next
-    files:
-      - '**/*'
-  cache:
-    paths:
-      - node_modules/**/*
-      - .pnpm-store/**/*
-EOT
+  bucket = aws_s3_bucket.fight_me_frontend.id
+  acl    = "public-read"
 }
 
-resource "aws_amplify_branch" "fight_me_frontend_amplify_app_main" {
-  app_id      = aws_amplify_app.fight_me_frontend_amplify_app.id
-  branch_name = "main"
+resource "aws_s3_object" "fight_me_frontend_object" {
+  key          = "index.html"
+  bucket       = aws_s3_bucket.fight_me_frontend.bucket
+  source       = ".next"
+  acl          = "public-read"
+  content_type = "text/html"
 }
 
-resource "aws_amplify_webhook" "fight_me_frontend_amplify_app_main_webhook" {
-  app_id      = aws_amplify_app.fight_me_frontend_amplify_app.id
-  branch_name = aws_amplify_branch.fight_me_frontend_amplify_app_main.branch_name
+resource "aws_s3_bucket_policy" "fight_me_frontend_bucket_policy" {
+  bucket = aws_s3_bucket.fight_me_frontend.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = ["s3:GetObject"]
+        Resource  = [aws_s3_bucket.fight_me_frontend.arn, "${aws_s3_bucket.fight_me_frontend.arn}/*"]
+      }
+    ]
+  })
 }
+
+# provider "github" {
+#   token = var.github_token
+#   owner = var.github_owner
+# }
+
+# resource "aws_amplify_app" "fight_me_frontend_amplify_app" {
+#   name       = var.amplify_app_name
+#   repository = "https://github.com/${var.github_owner}/${var.github_repository}"
+
+#   access_token = var.github_token
+
+#   enable_auto_branch_creation = true
+
+#   auto_branch_creation_patterns = [
+#     "*",
+#     "*/**",
+#   ]
+
+#   auto_branch_creation_config {
+#     enable_auto_build = true
+#   }
+
+#   custom_rule {
+#     source = "</^[^.]+$|\\.(?!(css|gif|ico|jpg|js|png|txt|svg|woff|ttf|map|json)$)([^.]+$)/>"
+#     status = "200"
+#     target = "/index.html"
+#   }
+
+#   build_spec = <<EOT
+# version: 1
+# frontend:
+#   phases:
+#     preBuild:
+#       commands:
+#         - npx pnpm install
+#     build:
+#       commands:
+#         - npx pnpm build
+#   artifacts:
+#     baseDirectory: .next
+#     files:
+#       - '**/*'
+#   cache:
+#     paths:
+#       - node_modules/**/*
+#       - .pnpm-store/**/*
+# EOT
+# }
+
+# resource "aws_amplify_branch" "fight_me_frontend_amplify_app_main" {
+#   app_id      = aws_amplify_app.fight_me_frontend_amplify_app.id
+#   branch_name = "main"
+# }
+
+# resource "aws_amplify_webhook" "fight_me_frontend_amplify_app_main_webhook" {
+#   app_id      = aws_amplify_app.fight_me_frontend_amplify_app.id
+#   branch_name = aws_amplify_branch.fight_me_frontend_amplify_app_main.branch_name
+# }
